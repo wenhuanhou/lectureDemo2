13fb8ef1ddd80f97d90985d56ffb8076855a104c
wenhuanhou <wenhuanh@metropolia.fi>
2024-08-22 11:20:46 +0300

    ok

--- Variable Demo.py
+++ Variable Demo.py
@@ -1,3 +1,4 @@
+"""
 intVarialbe=4
 floatVariable=4.5
 stringVariable= "software 1 is fun"
@@ -9,3 +10,26 @@ print(stringVariable)
 print(type(intVarialbe))
 print(type(floatVariable))
 print(type(stringVariable))
+
+intvariable=int(floatVariable)
+print("here is the int version of float variable",intVariable)
+
+shareOfloan=500.50/3
+print(shareOfloan)
+print(int(shareOfloan))
+"""
+######
+"""
+num1 =int(input("enter a number: "))
+num2 =float(input("enter another number: "))
+sum = num1 + num2
+
+print(sum)
+"""
+
+name = input("enter your name: ")
+school = input("enter your school: ")
+
+print("you are : ", name, "and your school name is:", school)
+print(f"your name is: {name}, and your school name is: {school}")
+
